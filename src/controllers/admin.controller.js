e0c710320d75fa2f562a7cdef9359149db379aea
Hassanali1112 <134091195+Hassanali1112@users.noreply.github.com>
2025-09-15 07:50:40 +0500

    check admin added for adding employee

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,8 +1,17 @@
 import {AddEmployee} from "../models/addEmployee.model.js"
+import { User } from "../models/users.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 export const addEmployee = asyncHandler( async(req, res)=>{
+  const isAdmin = await User.findById(req.user._id).select("-password -refreshToken")
+
+  console.log("admin : ",isAdmin.role)
+
+  if(isAdmin.role !== "admin"){
+    throw new ApiError(401, "Unauthorized to add employee")
+  }
+
   const {
     fullName,
     cnic,
--- src/routes/addEmployee.route.js
+++ src/routes/addEmployee.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { addEmployee } from "../controllers/admin.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/add-employee").post(addEmployee);
+router.route("/add-employee").post(verifyJwt,addEmployee);
 
 export default router;
